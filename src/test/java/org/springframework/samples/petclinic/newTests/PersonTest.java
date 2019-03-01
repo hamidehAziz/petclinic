970cec81f1c4b2895194664bf48daa8b4b9276b6
ilchinr <pcabro@gmail.com>
2019-02-28 19:00:16 -0500

    simple test for a person

---
+++ src/test/java/org/springframework/samples/petclinic/newTests/PersonTest.java
@@ -0,0 +1,35 @@
+package org.springframework.samples.petclinic.model;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PersonTest {
+
+    private Person person;
+
+    @Before
+    public void setup() {
+        person = new Person();
+    }
+
+    @Test
+    public void testLastNameGetSet() {
+        assertEquals(person.getLastName(), null);
+
+        person.setLastName("Doe");
+        assertEquals(person.getLastName(), "Doe");
+    }
+
+    @Test
+    public void testFirstNameGetSet() {
+        assertEquals(person.getFirstName(), null);
+
+        person.setFirstName("John");
+        assertEquals(person.getFirstName(), "John");
+    } 
+}
