000240dc4c6777a3441c30970e2e79eb3cb3dc95
ilchinr <pcabro@gmail.com>
2019-03-23 16:10:20 -0400

    added a forklift for Visits

--- src/main/java/org/springframework/samples/petclinic/forklift/Forklifter.java
+++ src/main/java/org/springframework/samples/petclinic/forklift/Forklifter.java
@@ -3,15 +3,19 @@ package org.springframework.samples.petclinic.forklift;
 import org.springframework.samples.petclinic.mysql.domain.Owner;
 import org.springframework.samples.petclinic.mysql.domain.Pet;
 import org.springframework.samples.petclinic.mysql.domain.Vet;
+import org.springframework.samples.petclinic.mysql.domain.Visit;
 import org.springframework.samples.petclinic.mysql.repo.MysqlOwnerRepository;
 import org.springframework.samples.petclinic.mysql.repo.MysqlPetRepository;
 import org.springframework.samples.petclinic.mysql.repo.MysqlVetRepository;
+import org.springframework.samples.petclinic.mysql.repo.MysqlVisitRepository;
 import org.springframework.samples.petclinic.postgres.domain.PostgresOwner;
 import org.springframework.samples.petclinic.postgres.domain.PostgresPet;
 import org.springframework.samples.petclinic.postgres.domain.PostgresVet;
+import org.springframework.samples.petclinic.postgres.domain.PostgresVisit;
 import org.springframework.samples.petclinic.postgres.repo.PostgresOwnerRepository;
 import org.springframework.samples.petclinic.postgres.repo.PostgresPetRepository;
 import org.springframework.samples.petclinic.postgres.repo.PostgresVetRepository;
+import org.springframework.samples.petclinic.postgres.repo.PostgresVisitRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
@@ -21,30 +25,33 @@ import java.util.List;
 public class Forklifter {
 
     private MysqlOwnerRepository mysqlOwnerRepository;
-    protected MysqlPetRepository mysqlPetRepository;
-    protected MysqlVetRepository mysqlVetRepository;
-
+    private MysqlPetRepository mysqlPetRepository;
+    private MysqlVetRepository mysqlVetRepository;
+    private MysqlVisitRepository mysqlVisitRepository;
 
     private PostgresOwnerRepository postgresOwnerRepository;
     private PostgresPetRepository postgresPetRepository;
     private PostgresVetRepository postgresVetRepository;
+    private PostgresVisitRepository postgresVisitRepository;
 
-    public Forklifter(MysqlOwnerRepository mysqlOwnerRepository, PostgresOwnerRepository postgresOwnerRepository, MysqlPetRepository mysqlPetRepository, PostgresPetRepository postgresPetRepository, MysqlVetRepository mysqlVetRepository, PostgresVetRepository postgresVetRepository) {
+    public Forklifter(MysqlOwnerRepository mysqlOwnerRepository, PostgresOwnerRepository postgresOwnerRepository, MysqlPetRepository mysqlPetRepository, PostgresPetRepository postgresPetRepository, MysqlVetRepository mysqlVetRepository, PostgresVetRepository postgresVetRepository, MysqlVisitRepository mysqlVisitRepository, PostgresVisitRepository postgresVisitRepository) {
         this.mysqlOwnerRepository = mysqlOwnerRepository;
         this.postgresOwnerRepository = postgresOwnerRepository;
         this.mysqlPetRepository = mysqlPetRepository;
         this.postgresPetRepository = postgresPetRepository;
         this.mysqlVetRepository = mysqlVetRepository;
         this.postgresVetRepository = postgresVetRepository;
+        this.mysqlVisitRepository = mysqlVisitRepository;
+        this.postgresVisitRepository = postgresVisitRepository;
     }
 
     public void forklift() {
         forkliftOwners();
         forkLiftPets();
         forkLiftVets();
+        forkLiftVisits();
         //TODO forklift specialities
         //TODO forklift types
-        //TODO forklift visits
     }
 
     public void forkliftOwners() {
@@ -94,6 +101,20 @@ public class Forklifter {
 
         }
 
+    }
+
+    public void forkLiftVisits(){
+        List<Visit> visits = mysqlVisitRepository.findAll();
+
+        PostgresVisit postgresVisit;
+
+        for (Visit visit : visits){
+
+            postgresVisit = new PostgresVisit();
+            postgresVisit.setDate(visit.getDate());
+            postgresVisit.setDescription(visit.getDescription());
+            postgresVisit.setPetId(visit.getPetId());
+        }
     }
 
 }
