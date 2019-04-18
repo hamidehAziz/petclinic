db77c34bb28fefedab98ea72f06fd4155997eb80
bizahmad <ahmad.baiazid@gmail.com>
2019-04-18 13:16:58 -0400

    add logging

--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -1,8 +1,7 @@
 package org.springframework.samples.petclinic.system;
 
-
-import jdk.nashorn.internal.objects.annotations.Getter;
-import jdk.nashorn.internal.objects.annotations.Setter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.samples.petclinic.system.toggles.Toggles;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,20 +11,24 @@ class WelcomeController {
 
     private int biligualPageCount;
     private int welcomePageCount;
+    private Logger logger = LogManager.getLogger(WelcomeController.class);
+
 
     @GetMapping("/")
     public String welcome() {
-        if (Toggles.newWelcomePage){
+        if (!Toggles.newWelcomePage){
             return "bilingualWelcome";
         }
         return "welcome";
     }
 
     public void incrementWelcomePageCounter(){
+        logger.info("regular welcome page count accessed: " + welcomePageCount);
         welcomePageCount++;
     }
 
     public void incrementBilingualPageCounter(){
+        logger.info("bilingual welcome page count accessed: " + biligualPageCount);
         biligualPageCount++;
     }
 
--- src/test/java/org/springframework/samples/petclinic/system/WelcomeControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/system/WelcomeControllerTest.java
@@ -1,27 +1,27 @@
 package org.springframework.samples.petclinic.system;
 
-import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.samples.petclinic.system.toggles.Toggles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = WelcomeControllerTest.class)
 public class WelcomeControllerTest {
 
+    private WelcomeController welcomeController;
+
+    @Before
+    public void setup(){
+        welcomeController = new WelcomeController();
+    }
+
     @Test
-    public void testRollBackWelcomePage(){
-        WelcomeController welcomeController = new WelcomeController();
+    public void testRollBackWelcomePage() {
         Toggles.newWelcomePage = true;
         assertEquals("bilingualWelcome", welcomeController.welcome());
 
@@ -33,19 +33,17 @@ public class WelcomeControllerTest {
     }
 
     @Test
-    public void testRandomWelcomePage(){
+    public void testRandomWelcomePage() {
         int iterations = 1000;
-        WelcomeController welcomeController = new WelcomeController();
 
-        for(int i = 0; i < iterations; i++){
+        for (int i = 0; i < iterations; i++) {
             Toggles.newWelcomePage = AssignRandomRequirement.getRandom(Boolean.TRUE);
 
             welcomeController.welcome();
             //new feature is on, see welcome page
-            if(Toggles.newWelcomePage) {
+            if (Toggles.newWelcomePage) {
                 welcomeController.incrementBilingualPageCounter();
-            }
-            else {
+            } else {
                 welcomeController.incrementWelcomePageCounter();
             }
         }
